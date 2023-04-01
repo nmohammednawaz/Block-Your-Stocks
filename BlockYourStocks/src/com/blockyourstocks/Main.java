676f45a28d678ff8a47f225a14ae52ca837ddd6c
nmohammednawaz <nmohammednawaz786@gmail.com>
2023-04-01 22:16:57 +0530

    Day5 task Completed

--- BlockYourStocks/src/com/blockyourstocks/Main.java
+++ BlockYourStocks/src/com/blockyourstocks/Main.java
@@ -4,32 +4,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import com.blockyourstocks.entities.Customer;
-import com.blockyourstocks.entities.Stock;
-import com.blockyourstocks.entities.Transaction;
-import com.blockyourstocks.exceptions.InvalidDetailsException;
-import com.blockyourstocks.exceptions.StockException;
-import com.blockyourstocks.exceptions.TransactionException;
-import com.blockyourstocks.services.CustomerService;
-import com.blockyourstocks.services.CustomerServiceImplement;
-import com.blockyourstocks.services.StockService;
-import com.blockyourstocks.services.StockServiceImplement;
-import com.blockyourstocks.services.TransactionService;
-import com.blockyourstocks.services.TransactionServiceImplement;
-import com.blockyourstocks.utilities.Admin;
-import com.blockyourstocks.utilities.FileExists;
-import com.blockyourstocks.utilities.IDGeneration;
+import com.blockyourstocks.entities.*;
+import com.blockyourstocks.exceptions.*;
+import com.blockyourstocks.services.*;
+import com.blockyourstocks.utilities.*;
+
 
 public class Main {
 	
-//	------------------------------------------ Admin Functionalities --------------------------------------------
+//	--------------------------------------- Admin Functionalities -------------------------------------------------
 	
-//	****************************************** Admin Login Method **********************************************
+//	***************************************** Admin Login Method ********************************************
 	public static void adminLogin(Scanner sc) throws InvalidDetailsException {
 
-		System.out.println("Enter the username");
+		System.out.println("Enter the username :");
 		String adminUserName = sc.next();
-		System.out.println("Enter the password");
+		System.out.println("Enter the password :");
 		String adminPassword = sc.next();
 		if (adminUserName.equals(Admin.username) && adminPassword.equals(Admin.password)) {
 			System.out.println("Hey..! Admin, Login successfull...");
@@ -38,8 +28,7 @@ public class Main {
 		}
 	}
 	
-	
-//	****************************************** Admin Add Stock Method ******************************************
+//	**************************************** Admin Add Stock Method *****************************************
 	public static String adminAddStock(Scanner sc, Map<Integer, Stock> stocks, StockService stockService) {
 
 		String str = null;
@@ -61,16 +50,13 @@ public class Main {
 
 	}
 	
-	
-//	****************************************** Admin View All Stock Method ******************************************
+//	************************************** Admin View All Stock Method **************************************
 	public static void adminViewAllStocks(Map<Integer, Stock> stocks, StockService stockService)
 			throws StockException {
 		stockService.viewAllStocks(stocks);
 	}
 	
-	
-	
-//	****************************************** Admin Delete Stock Method ******************************************
+//	*************************************** Admin Delete Stock Method ***************************************
 	public static void adminDeleteStock(Scanner sc, Map<Integer, Stock> stocks, StockService stockService)
 			throws StockException {
 
@@ -79,8 +65,7 @@ public class Main {
 		stockService.deleteStock(id, stocks);
 	}
 	
-	
-//	****************************************** Admin Update Stock Method *******************************************
+//	*************************************** Admin Update Stock Method ***************************************
 	public static String adminUpdateStock(Scanner sc, Map<Integer, Stock> stocks, StockService stockService)
 			throws StockException {
 		String result = null;
@@ -108,8 +93,7 @@ public class Main {
 		return result;
 	}
 	
-	
-//	****************************************** Admin View All Customers Method *******************************************
+//	****************************************** Admin View All Customers Method ******************************************
 	public static void adminViewAllCustomers(Map<String, Customer> customers, CustomerService cusService)
 			throws StockException {
 		List<Customer> list = cusService.viewAllCustomers(customers);
@@ -118,21 +102,33 @@ public class Main {
 			System.out.println(c);
 		}
 	}
+
+//	************************************ Admin View All Transaction Method **********************************
+	public static void adminViewAllTransactions(List<Transaction> transactions, TransactionService transactionService)
+			throws TransactionException {
+		List<Transaction> allTransactions = transactionService.viewAllTransactions(transactions);
+
+		for (Transaction tr : allTransactions) {
+			System.out.println(tr);
+		}
+
+	}
 	
+//	*************************************** Admin Functionality method **************************************
 	private static void adminFunctionality(Scanner sc, Map<Integer, Stock> stocks, Map<String, Customer> customers,
 			List<Transaction> transactions) throws InvalidDetailsException, StockException, TransactionException {
 		
-//		Admin login
+//		Caling Admin login Method
 		adminLogin(sc);
 
 		StockService stockService = new StockServiceImplement();
 		CustomerService cusService = new CustomerServiceImplement();
-		TransactionService trnsactionService = new TransactionServiceImplement();
+		TransactionService transactionService = new TransactionServiceImplement();
 		
 		int choice = 0;
 		try {
 			do {
-				System.out.println("Please select the preference");
+				System.out.println("Please select your preference");
 				System.out.println("1. add the Stock");
 				System.out.println("2. view all the stocks");
 				System.out.println("3. delete the stock");
@@ -159,10 +155,9 @@ public class Main {
 					break;
 				case 5:
 					adminViewAllCustomers(customers, cusService);
-
 					break;
 				case 6:
-//					adminViewAllTransactions(transactions, trnsactionService);
+					adminViewAllTransactions(transactions, transactionService);
 					break;
 				case 7:
 					System.out.println("admin has successfully logout");
@@ -176,8 +171,166 @@ public class Main {
 			System.out.println(e.getMessage());
 		}
 	}
+
+//	====================================== End Of Admin Fucntionalities ===========================================
 	
 	
+//	---------------------------------------- Customer Functionalities ---------------------------------------------
+	
+//	**************************************** Customer Register Method ****************************************
+	public static void customerRegister(Scanner sc, Map<String, Customer> customers) throws DuplicateDataException {
+		System.out.println("please enter the following details to Register");
+		System.out.println("please enter the user name");
+		String name = sc.next();
+		System.out.println("Enter the password");
+		String pass = sc.next();
+		System.out.println("enter the address");
+		String address = sc.next();
+		System.out.println("Enter the email id");
+		String email = sc.next();
+		System.out.println("Enter the balance to be added into the wallet");
+		double balance = sc.nextDouble();
+		Customer cus = new Customer(balance, name, pass, address, email);
+
+		CustomerService cusService = new CustomerServiceImplement();
+		cusService.register(cus, customers);
+		System.out.println("Registration Successfull...!");
+
+	}
+	
+//	***************************************** Customer Login Method ******************************************
+	public static void customerLogin(String email,String pass, Map<String, Customer> customers, CustomerService cusService)
+			throws InvalidDetailsException {
+		cusService.login(email, pass,customers);
+		System.out.println("Customer has successfully login");
+
+	}
+	
+//	************************************** Customer View All Stock Method ************************************
+	public static void customerViewAllStocks(Map<Integer, Stock> stocks, StockService stockService)
+			throws StockException {
+		stockService.viewAllStocks(stocks);
+	}
+
+//	**************************************** Customer Buy Stock Method ***************************************
+	public static String customerBuyStock(Scanner sc, String email, Map<Integer, Stock> stocks,
+			Map<String, Customer> customers, List<Transaction> transactions, CustomerService cusService)
+			throws InvalidDetailsException, StockException {
+		System.out.println("Enter the Stock id");
+		int id = sc.nextInt();
+		System.out.println("enter the quantity you want to buy");
+		int qty = sc.nextInt();
+		cusService.buyStock(id, qty, email, stocks, customers, transactions);
+
+		return "You have successfully bought the product";
+
+	}
+	
+//	**************************************** Customer Add Money Method ***************************************
+	public static String customerAddMoneyToWallet(Scanner sc, String email, Map<String, Customer> customers,
+			CustomerService cusService) {
+		System.out.println("please enter the amount");
+		double money = sc.nextDouble();
+		boolean added = cusService.addMoneyToWallet(money, email, customers);
+
+		return "Amount of " + money + " successfully added to your wallet";
+	}
+	
+//	**************************************** Customer view wallet Method *************************************
+	public static double customerViewWalletBalance(String email, Map<String, Customer> customers,
+			CustomerService cusService) {
+		double walletBalance = cusService.viewWalletBalance(email, customers);
+		return walletBalance;
+	}
+	
+//	**************************************** Customer view profile Method ************************************
+	public static void customerViewMyDetails(String email, Map<String, Customer> customers,
+			CustomerService cusService) {
+		Customer cus = cusService.viewCustomerDetails(email, customers);
+		System.out.println("name : " + cus.getUsername());
+		System.out.println("address : " + cus.getAddress());
+		System.out.println("email : " + cus.getEmail());
+		System.out.println("wallet balance : " + cus.getWalletBalance());
+	}
+	
+//	********************************* Customer Transaction Details Method ************************************
+	public static void customerViewCustomerTransactions(String email, List<Transaction> transactions,
+			TransactionService trnsactionService) throws TransactionException {
+		List<Transaction> myTransactions = trnsactionService.viewCustomerTransactions(email, transactions);
+
+		for (Transaction tr : myTransactions) {
+			System.out.println(tr);
+		}
+	}
+	
+//	************************************* Customer Functionality method **************************************
+	public static void customerFunctionality(Scanner sc, Map<String, Customer> customers,
+			Map<Integer, Stock> stocks, List<Transaction> transactions)
+			throws InvalidDetailsException, TransactionException {
+
+		CustomerService cusService = new CustomerServiceImplement();
+		StockService stockService = new StockServiceImplement();
+		TransactionService transactionService = new TransactionServiceImplement();
+
+		// Customer login
+		System.out.println("please enter the following details to login");
+		System.out.println("please enter the email");
+		String email = sc.next();
+		System.out.println("Enter the password");
+		String pass = sc.next();
+		customerLogin(email,pass, customers, cusService);
+
+		try {
+			int choice = 0;
+			do {
+				System.out.println("Please select your preference");
+				System.out.println("1. view all products");
+				System.out.println("2. buy a product");
+				System.out.println("3. add money to a wallet");
+				System.out.println("4. view wallet balance");
+				System.out.println("5. view my details");
+				System.out.println("6. view my transactions");
+				System.out.println("7. logout");
+				choice = sc.nextInt();
+
+				switch (choice) {
+				case 1:
+					customerViewAllStocks(stocks, stockService);
+					break;
+				case 2:
+					String result = customerBuyStock(sc, email, stocks, customers, transactions, cusService);
+					System.out.println(result);
+					break;
+				case 3:
+					String moneyAdded = customerAddMoneyToWallet(sc, email, customers, cusService);
+					System.out.println(moneyAdded);
+					break;
+				case 4:
+					double walletBalance = customerViewWalletBalance(email, customers, cusService);
+					System.out.println("Wallet balance is: " + walletBalance);
+					break;
+				case 5:
+					customerViewMyDetails(email, customers, cusService);
+					break;
+				case 6:
+					customerViewCustomerTransactions(email, transactions, transactionService);
+					break;
+				case 7:
+					System.out.println("you have successsfully logout");
+					break;
+				default:
+					System.out.println("invalid choice");
+					break;
+				}
+
+			} while (choice <= 6);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+//	=================================== End Of Customer Functionalities Method =====================================
 	
 	
 	
@@ -199,19 +352,19 @@ public class Main {
 		try {
 			int choice = 0;
 			do {
-				System.out.println("Please enter your preference, " + " '1' --> Admin, '2' --> Customer"
-				+ ", '0' --> for exit");
+				System.out.println("Please enter your preference,");
+				System.out.println("1. Admin \n2. Customer Login \n3. Customer Register \n0. for exit");
 				choice = sc.nextInt();
 				switch (choice) {
 				case 1:
 					adminFunctionality(sc, stocks, customers, transactions);
 					break;
 				case 2:
-//					customerFunctionality(sc, customers, stocks, transactions);
+					customerFunctionality(sc, customers, stocks, transactions);
 					break;
 
 				case 3:
-//					customerSignup(sc, customers);
+					customerRegister(sc, customers);
 					break;
 
 				case 0:
@@ -231,4 +384,5 @@ public class Main {
 		} 
 	}
 
+//	============================================= End Of MAin Method ================================================
 }
